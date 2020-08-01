ce89991c3bc5f281dffe253a3181d31ec7fa145a
juan sebastian herrera <juan.herreraca@campusucc.edu.co>
2020-07-31 23:33:13 -0500

    Display property

---
+++ toogle.js
@@ -0,0 +1,9 @@
+function toggleMain(){
+    var main = document.getElementById("main");
+    if (main.style.display === "flex") {
+        main.style.display = "none"
+    }
+    else{
+      main.style.display = "flex"
+    }
+  }
